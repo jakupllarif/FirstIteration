ab677d54858170f014980eaa0e6eb1bee184d3d7
ndurso <durson@sacredheart.edu>
2012-12-03 15:06:59 -0500

    test user data

---
+++ db/uservalues.sql
@@ -0,0 +1,21 @@
+INSERT INTO `javadb`.`users` (firstName, address, username, password, email, city, state, zipcode, lastName, middleName, datetimelastlogin, wrongtries, lockedout)
+VALUES
+(
+
+'Nicole',
+ '24 danfred st',
+ 'ndurso', 
+'password', 
+'durson@sacremailedheart.edu',
+'danbury', 
+
+'ct',
+ '06810',
+'durso', 
+'l', 
+ CURRENT_TIMESTAMP,
+ '0',
+'0'
+);
+
+
