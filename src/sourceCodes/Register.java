1dac93526ae333cd721b3cf5a55cde8cc755b045
ndurso <durson@sacredheart.edu>
2012-12-03 13:22:04 -0500

    added some necessary imports

--- src/sourceCodes/Register.java
+++ src/sourceCodes/Register.java
@@ -1,6 +1,13 @@
+
+
 package sourceCodes;
 
 import java.util.concurrent.atomic.AtomicReferenceArray;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 
--- src/sourceCodes/User.java
+++ src/sourceCodes/User.java
@@ -1,5 +1,11 @@
 package sourceCodes;
 
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class User {
 	
 	String firstName;
